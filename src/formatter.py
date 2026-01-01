4b7b263835e5db25fab88c409789a35fb0b38f5f
Avery Hankins <164415993+avery-hankins@users.noreply.github.com>
2026-01-01 15:32:53 -0800

    tipo: correct command prefix

--- src/formatter.py
+++ src/formatter.py
@@ -7,7 +7,7 @@ def featured_embed(album_details: dict) -> discord.Embed:
     embed.title = "Featured:"
     embed.description = f"[{album_details['album']}](<{album_details['album_url']}>)\n by [{album_details['artist_name']}](<{album_details['artist_url']}>)\n\nWeekly albums from {album_details['member_l']}"
     embed.set_thumbnail(url=album_details["cover_url"])
-    embed.set_footer(text="View your featured history with '.featuredlog'")
+    embed.set_footer(text="View your featured history with '!featuredlog'")
 
     return embed
 
